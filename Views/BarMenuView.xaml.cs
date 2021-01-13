4d0073121c279e474383e8ac3a6d95b5a4f626b0
Felix Rizzolli <felix.rizzolli@outlook.de>
2021-01-13 08:12:10 +0100

    BarMenu Kassa Eingabe
    Auf der Seite BarMenu kann nun die Kassa bedient werden. Die berechnung funktioniert aber noch nicht.

--- Views/BarMenuView.xaml.cs
+++ Views/BarMenuView.xaml.cs
@@ -138,307 +138,64 @@ namespace ThirstyTap.Views
             //#endregion
         }
 
-        #region Btn_click_1
-        private void Btn_click_1(object sender, RoutedEventArgs e)
+        private void BTN_INPUT_Click(object sender, RoutedEventArgs e)
         {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 1;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-
+            Button button = (Button)sender;
+            tb_kassa_output_akt.Text = (tb_kassa_output_akt.Text == "0" && button.Content.ToString() != ",")
+                ? button.Content.ToString()
+                : tb_kassa_output_akt.Text + button.Content;
         }
-        #endregion
-
-        #region Btn_click_2
-        private void Btn_click_2(object sender, RoutedEventArgs e)
+        private void BTN_OPERATOR_Click(object sender, RoutedEventArgs e)
         {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 2;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_3
-        private void Btn_click_3(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 3;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_4
-        private void Btn_click_4(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 4;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_5
-        private void Btn_click_5(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 5;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_6
-        private void Btn_click_6(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 6;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_7
-        private void Btn_click_7(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 7;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_8
-        private void Btn_click_8(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 8;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_9
-        private void Btn_click_9(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 9;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_click_0
-        private void Btn_click_0(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            int num = 0;
-
-            //tb_kassa.Text = funcDLL.Func.NumPadIns(tb, num);
-        }
-        #endregion
-
-        #region Btn_del
-        private void Btn_del(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-
-            var index = lv_Users.Items.IndexOf(lv_Users.SelectedItem);
-            //MessageBox.Show("Index: " + index);
-            if (index == -1)
+            Button button = (Button)sender;
+            switch (button.Content.ToString())
             {
-                if (tb.Length > 1)
-                {
-                    tb = tb.Substring(0, tb.Length - 1);
-                }
-                else
-                {
-                    tb = "0";
-                }
-                tb_kassa.Text = tb.ToString();
+                case "+":
+                    lbl_kassa_output_full.Content = lbl_kassa_output_full.Content + tb_kassa_output_akt.Text + "+";
+                    break;
+                case "-":
+                    lbl_kassa_output_full.Content = lbl_kassa_output_full.Content + tb_kassa_output_akt.Text + "-";
+                    break;
+                case "*":
+                    lbl_kassa_output_full.Content = lbl_kassa_output_full.Content + tb_kassa_output_akt.Text + "*";
+                    break;
+                case "/":
+                    lbl_kassa_output_full.Content = lbl_kassa_output_full.Content + tb_kassa_output_akt.Text + "/";
+                    break;
             }
-            else
-            {
-                double priceOne;
-                string lblTotal;
-                var selectedStockObject = lv_Users.SelectedItems[0] as MyItem;
-                if (selectedStockObject == null)
-                {
-                    return;
-                }
-
-                priceOne = selectedStockObject.Price;
-                lblTotal = lbl_totale.Content.ToString();
-                //lbl_totale.Content = funcDLL.Func.TotalSumMinus(priceOne, lblTotal);
-
-                
-                lv_Users.Items.RemoveAt(index);
-            }
-            
+            tb_kassa_output_akt.Clear();
         }
-        #endregion
-
-        #region Btn_multi
-        private void Btn_multi(object sender, RoutedEventArgs e)
+        private void BTN_FUNC_Click(object sender, RoutedEventArgs e)
         {
-            if(tb_kassa.Text == "0")
+            Button button = (Button)sender;
+            switch (button.Content.ToString())
             {
-                lv_Users.SelectAll();
-            }
-            else
-            {
-                quantity = int.Parse(tb_kassa.Text);
-                tb_kassa.Text = "0";
-            }
-
-        }
-        #endregion
-
-        #region Btn_minus
-        private void Btn_minus(object sender, RoutedEventArgs e)
-        {
-
-        }
-        #endregion
-
-        #region Btn_plus
-        private void Btn_plus(object sender, RoutedEventArgs e)
-        {
-
-        }
-        #endregion
-
-        #region Btn_click_result
-        private void Btn_click_result(object sender, RoutedEventArgs e)
-        {
-
-        }
-        #endregion
-
-        #region Btn_click_comma
-        private void Btn_click_comma(object sender, RoutedEventArgs e)
-        {
-            //Variablen
-            string tb = tb_kassa.Text.ToString();
-            String comm = ",";
-
-            if (tb == "")
-            {
-                tb_kassa.Text = comm.ToString();
-            }
-            else if (tb != "")
-            {
-                tb_kassa.Text += comm.ToString();
+                case "DEL":
+                    tb_kassa_output_akt.Text = tb_kassa_output_akt.Text.Remove(tb_kassa_output_akt.Text.Length - 1);
+                    if (tb_kassa_output_akt.Text == "")
+                    {
+                        tb_kassa_output_akt.Text = "0";
+                    }
+                    break;
+                case "TOTALE":
+                    break;
             }
         }
-        #endregion
 
-        #region Btn_varie_click
         private void Btn_varie_click(object sender, RoutedEventArgs e)
         {
-            //Variable
-            string tb =  tb_kassa.Text.ToString();
-            char[] separator = {'.',','};
-            Int32 count = 2;
-            Double priceTb;
-            String lblTotal;
 
-            //Todo Null bei einstelliger Decimal hinzufügen
-            String[] strlist = tb.Split(separator, count, StringSplitOptions.None);
-            
-            if (tb.Contains(",") == false)
-            {
-                priceTb = Convert.ToDouble(strlist[0] + ",0");
-            }
-            else
-            {
-                priceTb = Convert.ToDouble(strlist[0] + "," + strlist[1]);
-            }
-            priceTb = priceTb * quantity;
-
-            this.lv_Users.Items.Add(new MyItem { Product = "Varie 10%", Quantity = quantity, Price = priceTb });
-
-            lblTotal = lbl_totale.Content.ToString();
-
-            //lbl_totale.Content = funcDLL.Func.TotalSum(priceTb, lblTotal);
-
-
-
-
-            //Textbox zurücksetzen
-            tb_kassa.Clear();
-            tb_kassa.Text = "0";
-            quantity = 1;
         }
-        #endregion
 
-        #region Lvi_loeschen
+        private void Lvi_alleloeschen(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void Lvi_loeschen(object sender, RoutedEventArgs e)
         {
-            var index = lv_Users.Items.IndexOf(lv_Users.SelectedItem);
-            
-            double priceOne;
-            string lblTotal;
-            var selectedStockObject = lv_Users.SelectedItems[0] as MyItem;
-            if (selectedStockObject == null)
-            {
-                return;
-            }
-
-            priceOne = selectedStockObject.Price;
-            lblTotal = lbl_totale.Content.ToString();
-            //lbl_totale.Content = funcDLL.Func.TotalSumMinus(priceOne, lblTotal);
-
-            
-            lv_Users.Items.RemoveAt(index);
-            
 
         }
 
-            private void Lvi_alleloeschen(object sender, RoutedEventArgs e)
-        {
-            lv_Users.Items.Clear();
-            lbl_totale.Content = "0";
-        }
-        #endregion
-
-        private void Btn_2_1(object sender, RoutedEventArgs e)
-        {
-            int group = 2;
-            int prod = 1;
-            char[] separator = { '.', ',' };
-            Double priceTb = 0.0;
-            String lblTotal;
-            string prodName = "";
-
-            //prodName = funcDLL.Func.getProdName(group, prod, prodName);
-            //priceTb = funcDLL.Func.getPrice(group, prod, priceTb) * quantity;
-
-            this.lv_Users.Items.Add(new MyItem { Product = prodName, Quantity = quantity, Price = priceTb });
-
-            lblTotal = lbl_totale.Content.ToString();
-
-            //lbl_totale.Content = funcDLL.Func.TotalSum(priceTb, lblTotal);
-            quantity = 1;
-        }
-    }
-
-    internal class MyItem
-    {
-        public String Product { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
     }
 }
